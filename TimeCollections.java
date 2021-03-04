966272d78e150a9700c429a76ef0c6f8b3b02be0
Elliot Parker <elliotccp@protonmail.com>
2021-03-04 13:43:36 +1100

    Add task 4 content

--- TimeCollections.java
+++ TimeCollections.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.LinkedList;
 
 public class TimeCollections {
     private String testName;
@@ -44,17 +46,78 @@ public class TimeCollections {
     }
 
     public static void main(String[] args) {
+        Integer n = 100000; //Change to 1000000 when ready.
+        
+        //Test 1
+
         TimeCollections test1 = new TimeCollections("staticArray");
-        Integer n = 1000000;
         RandomPerson[] test1Array = new RandomPerson[n];
 
         test1.setStartTime(new Date());
-        for (int i=0; i<n; ++i)
-		{
-			RandomPerson p = new RandomPerson();
-			test1Array[i] = p;
+        for (int i=0; i<n; ++i) {
+			test1Array[i] = new RandomPerson();
 		}
         test1.setEndTime(new Date());
         System.out.println(test1);
+        
+        //Test 2
+
+        TimeCollections test2 = new TimeCollections("arrayListAdd");
+        ArrayList<RandomPerson> test2Array = new ArrayList<RandomPerson>();
+
+        test2.setStartTime(new Date());
+        for (int i=0; i<n; ++i) {
+			test2Array.add(new RandomPerson());
+		}
+        test2.setEndTime(new Date());
+        System.out.println(test2);
+
+        //Test 3
+
+        TimeCollections test3 = new TimeCollections("arrayListAddBeginning");
+        ArrayList<RandomPerson> test3Array = new ArrayList<RandomPerson>();
+
+        test3.setStartTime(new Date());
+        for (int i=0; i<n; ++i) {
+			test3Array.add(0,new RandomPerson());
+		}
+        test3.setEndTime(new Date());
+        System.out.println(test3);
+
+        //Test 4
+
+        TimeCollections test4 = new TimeCollections("arrayListPreSized200k");
+        ArrayList<RandomPerson> test4Array = new ArrayList<RandomPerson>(n/5); //For testing. n/5 at default is 200k, as required.
+
+        test4.setStartTime(new Date());
+        for (int i=0; i<n; ++i) {
+			test4Array.add(new RandomPerson());
+		}
+        test4.setEndTime(new Date());
+        System.out.println(test4);
+
+        //Test 5
+
+        TimeCollections test5 = new TimeCollections("linkedList");
+        LinkedList<RandomPerson> test5Array = new LinkedList<RandomPerson>();
+
+        test5.setStartTime(new Date());
+        for (int i=0; i<n; ++i) {
+			test5Array.add(new RandomPerson());
+		}
+        test5.setEndTime(new Date());
+        System.out.println(test5);
+
+        //Test 6
+
+        TimeCollections test6 = new TimeCollections("linkedListAddBeginning");
+        LinkedList<RandomPerson> test6Array = new LinkedList<RandomPerson>();
+
+        test6.setStartTime(new Date());
+        for (int i=0; i<n; ++i) {
+			test6Array.add(0,new RandomPerson());
+		}
+        test6.setEndTime(new Date());
+        System.out.println(test6);
     }
 }
